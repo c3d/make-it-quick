c3d/make-it-quick

commit 96cad841f883efe6ad38917e5482f1b633daf86a

Reorder numbering for shared libraries with extensions
On systems that rely on extensions to identify a shared library
(macOS and cygwin), then move from "libfoo.dll.1" to "libfoo.1.dll"

config.cygwin.mk
@@ -32,5 +32,6 @@ OBJ_EXT=	.obj
 
 # Because of the above, we need to put the number before the extesion
 # e.g. where Linux would have libfoo.so.1.3.2, cygwin has libfoo.1.3.2.dll
-MIQ_NOINSTALL=	$(@:%.install_dll=%)
-MIQ_DLLNAME=	$(MIQ_NOINSTALL:%$(DLL_EXT)=%$(PRODUCTS_VERSION:%=.$(MIQ_V_VERSION))$(DLL_EXT))
+MIQ_DLLNAME=	$(MIQ_SOBASE:%$(DLL_EXT)=%$(PRODUCTS_VERSION:%=.$(MIQ_V_VERSION))$(DLL_EXT))
+MIQ_SONAME=	$(MIQ_SOBASE:%$(DLL_EXT)=%)$(MIQ_V_MAJOR:%=.%)$(DLL_EXT)
+MIQ_SONAME_OPT=	$(PRODUCTS_VERSION:%=-Wl,-soname -Wl,$(MIQ_SONAME))

config.macosx-clang.mk
@@ -30,8 +30,8 @@ DLL_EXT=	.dylib
 
 # For macOS, the convention is to put the version number before extension,
 # e.g. where Linux would have libfoo.so.1.3.2, macOS has libfoo.1.3.2.dylib
-MIQ_NOINSTALL=	$(@:%.install_dll=%)
-MIQ_DLLNAME=	$(MIQ_NOINSTALL:%$(DLL_EXT)=%$(PRODUCTS_VERSION:%=.$(MIQ_V_VERSION))$(DLL_EXT))
+MIQ_DLLNAME=	$(MIQ_SOBASE:%$(DLL_EXT)=%$(PRODUCTS_VERSION:%=.$(MIQ_V_VERSION))$(DLL_EXT))
+MIQ_SONAME=	$(MIQ_SOBASE:%$(DLL_EXT)=%)$(MIQ_V_MAJOR:%=.%)$(DLL_EXT)
 MIQ_SONAME_OPT=	$(PRODUCTS_VERSION:%=-Wl,-install_name -Wl,$(MIQ_SONAME))
 
 # On MacOSX, we will use basic frameworks e.g. for string and filesystem functions

config.gnu.mk
@@ -101,7 +101,7 @@ DLL_PFX=	lib
 #------------------------------------------------------------------------------
 
 MIQ_SOBASE=		$(@F:%.install_dll=%)
-MIQ_SONAME=		$(@F:%.install_dll=%)$(MIQ_V_MAJOR:%=.%)
+MIQ_SONAME=		$(MIQ_SOBASE)$(MIQ_V_MAJOR:%=.%)
 MIQ_DLLNAME=		$(@:%.install_dll=%)$(PRODUCTS_VERSION:%=.$(MIQ_V_VERSION))
 
 # Conversion to libttool input

commit db37db79ccd27527d9d27d0589320ceebfffe1f3

Fix regression in 'make test'
The regression was introduced in 96cad841f883efe6ad38917e5482f1b633daf86a
on Cygwin and macOS. The MIQ_DLLNAME variable used $(@F) instead of $@.

The Jenkins setup failed to see the problem because it rebuilt
on top of existing directories that were pruned with existing libraries.

Added a second build after `git clean -dfx` in Jenkins to catch that.

config.cygwin.mk
@@ -32,6 +32,7 @@ OBJ_EXT=	.obj
 
 # Because of the above, we need to put the number before the extesion
 # e.g. where Linux would have libfoo.so.1.3.2, cygwin has libfoo.1.3.2.dll
-MIQ_DLLNAME=	$(MIQ_SOBASE:%$(DLL_EXT)=%$(PRODUCTS_VERSION:%=.$(MIQ_V_VERSION))$(DLL_EXT))
+MIQ_DLLBASE=    $(@:%.install_dll=%)
+MIQ_DLLNAME=	$(MIQ_DLLBASE:%$(DLL_EXT)=%$(PRODUCTS_VERSION:%=.$(MIQ_V_VERSION))$(DLL_EXT))
 MIQ_SONAME=	$(MIQ_SOBASE:%$(DLL_EXT)=%)$(MIQ_V_MAJOR:%=.%)$(DLL_EXT)
 MIQ_SONAME_OPT=	$(PRODUCTS_VERSION:%=-Wl,-soname -Wl,$(MIQ_SONAME))

config.macosx-clang.mk
@@ -30,7 +30,8 @@ DLL_EXT=	.dylib
 
 # For macOS, the convention is to put the version number before extension,
 # e.g. where Linux would have libfoo.so.1.3.2, macOS has libfoo.1.3.2.dylib
-MIQ_DLLNAME=	$(MIQ_SOBASE:%$(DLL_EXT)=%$(PRODUCTS_VERSION:%=.$(MIQ_V_VERSION))$(DLL_EXT))
+MIQ_DLLBASE=    $(@:%.install_dll=%)
+MIQ_DLLNAME=	$(MIQ_DLLBASE:%$(DLL_EXT)=%$(PRODUCTS_VERSION:%=.$(MIQ_V_VERSION))$(DLL_EXT))
 MIQ_SONAME=	$(MIQ_SOBASE:%$(DLL_EXT)=%)$(MIQ_V_MAJOR:%=.%)$(DLL_EXT)
 MIQ_SONAME_OPT=	$(PRODUCTS_VERSION:%=-Wl,-install_name -Wl,$(MIQ_SONAME))
 