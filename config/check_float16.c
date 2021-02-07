a4c6a19e453a070ac95b7aee626328859fa3030a
Christophe de Dinechin <christophe@dinechin.org>
2021-02-07 01:15:39 +0100

    Add configuration check for _Float16
    Signed-off-by: Christophe de Dinechin <christophe@dinechin.org>

---
+++ config/check_float16.c
@@ -0,0 +1,44 @@
+// *****************************************************************************
+// check_float16.c                                       make-it-quick project
+// *****************************************************************************
+//
+// File description:
+//
+//     Check if _Float16 is defined by <stdlib.h> or <stdint.h>
+//
+//
+//
+//
+//
+//
+//
+//
+// *****************************************************************************
+// This software is licensed under the GNU General Public License v3
+// (C) 2018-2019, Christophe de Dinechin <christophe@dinechin.org>
+// *****************************************************************************
+// This file is part of make-it-quick
+//
+// make-it-quick is free software: you can r redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// make-it-quick is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with make-it-quick, in a file named COPYING.
+// If not, see <https://www.gnu.org/licenses/>.
+// *****************************************************************************
+
+#include <stdlib.h>
+#include <stdint.h>
+
+int main()
+{
+    _Float16 x = 0.0;
+    return x == x && sizeof(_Float16) == sizeof(short) ? 0 : 1;
+}
