8ce736a0841efc09ef58f9ca9fda2d06e6a710d7
Christophe de Dinechin <christophe@dinechin.org>
2021-11-26 18:20:33 +0100

    tests: Time to change the test output to mention 'make-it-quick'
    The project is no longer called 'build' (and has not been in a long while)
    
    Signed-off-by: Christophe de Dinechin <christophe@dinechin.org>

--- example/hello.cpp
+++ example/hello.cpp
@@ -1,34 +1,34 @@
 // ****************************************************************************
-//  hello.cpp                                                    make-it-quick 
+//  hello.cpp                                                    make-it-quick
 // ****************************************************************************
-// 
+//
 //   File Description:
-// 
+//
 //     A simple "hello-world" test for make-it-quick
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+//
+//
+//
+//
+//
+//
+//
+//
 // ****************************************************************************
 //   (C) 2019 Christophe de Dinechin <christophe@dinechin.org>
 //   This software is licensed under the GNU General Public License v3
 // ****************************************************************************
 //   This file is part of make-it-quick.
-// 
+//
 //   make-it-quick is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation, either version 3 of the License, or
 //   (at your option) any later version.
-// 
+//
 //   Foobar is distributed in the hope that it will be useful,
 //   but WITHOUT ANY WARRANTY; without even the implied warranty of
 //   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //   GNU General Public License for more details.
-// 
+//
 //   You should have received a copy of the GNU General Public License
 //   along with make-it-quick.  If not, see <https://www.gnu.org/licenses/>.
 // ****************************************************************************
@@ -54,9 +54,9 @@ extern "C" int lib2_bar(void);
 int main()
 {
 #if HAVE_IOSTREAM
-    std::cout << "You successfully built using build\n";
+    std::cout << "You successfully built using make-it-quick\n";
 #elif HAVE_STDIO_H
-    printf("You successfully built using build (without iostream)\n");
+    printf("You successfully built using make-it-quick (without iostream)\n");
 #else
 #warning "Building without <iostream> or <stdio.h>. Cross-compiling?"
 #endif // HAVE_IOSTREAM
